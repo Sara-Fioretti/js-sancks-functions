b13fb97c6a4e65941c34adfcdb20307bc8ec9ab0
Sara-Fioretti <fiorettisara22@gmail.com>
2026-02-09 11:56:29 +0100

    First commit

---
+++ ex-classe/script.js
@@ -0,0 +1,28 @@
+/*ESERCIZIO 1
+ Creare una funzione che accetti come parametro un array e trovi il numero più grande all'interno
+*/
+
+/*ESERCIZIO 2
+Creare una funzione che accetti come parametro un array e trovi il numero più piccolo all'interno
+*/
+
+/*ESERCIZIO 3
+Creare una funzione che accetti come parametro un array e un numero e ritorna la posizione del numero nell'array o, se non c'è, ritorna -1
+*/
+
+/*ESERCIZIO 4
+Creare una funzione che accetti come parametro un array e ritorna la somma degli elementi dell'array
+*/
+
+/*ESERCIZIO 5
+Creare una funzione che inverta una stringa passata come argomento
+*/
+
+/*ESERCIZIO 6
+Creare una funzione che inverta un array passato come argomento
+*/
+
+/*ESERCIZIO 7
+Creare una funzione che conti le occorrenze di un numero in un array. 
+La funzione accetta come parametro un array e un numero e ritorna la quantità di volte in cui il numero è presente nell'array, ritorna -1 se non c'è
+*/
