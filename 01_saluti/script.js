3401dd6dfc932c23faae7a607b56752d848281e3
Sara-Fioretti <fiorettisara22@gmail.com>
2026-02-09 12:12:08 +0100

    ES.01

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,13 +1,13 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
-
 const userName = 'Mario';
 
-
 // Dichiara la funzione qui.
-
+function saluto (username){
+    return ("Ciao " + userName)
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+saluto("Mario")
+console.log (saluto("Mario"))
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
