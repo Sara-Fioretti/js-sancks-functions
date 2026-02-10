136e7023d09e03a11907f6c208edf49562548bb9
Sara-Fioretti <fiorettisara22@gmail.com>
2026-02-10 17:29:58 +0100

    ES 03

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,22 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
+/*PROCEDIMENTO
+1) Prendo ogni elemento della stringa usando un ciclo for
+2) Prendo l'iniziale di ogni elemento 
+3) If l'iniziale == lettera, stampo elemento
+*/
 
 // Dichiara la funzione qui.
-
-
+function inizialiParole (nomi, lettera){
+    for (let i=0; i<nomi.length; i++){
+        if(nomi[i].charAt(0)==lettera){
+            console.log (nomi[i])
+        }
+    }
+}
 // Invoca la funzione qui e stampa il risultato in console
-
+inizialiParole (names, "A")
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
