4ea4adb50fe0e0dd8b09fd218eb544a37e3eac8e
Sara-Fioretti <fiorettisara22@gmail.com>
2026-02-10 18:13:04 +0100

    ES 05

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,19 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function saluto (nome){
+    let date= new Date ();
+    let time = date.getHours();
+    if(time<= 13){
+        console.log("Buongiorno " + nome )
+    }else if(time<= 17){
+        console.log("Buon pomeriggio " + nome )
+    }else{
+        console.log("Buonasera " + nome )
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+let risultato = saluto(name)
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
