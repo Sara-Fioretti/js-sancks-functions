1ff964ad17a8d76da86f018ecdea20741054e3a2
Sara-Fioretti <fiorettisara22@gmail.com>
2026-02-10 17:23:19 +0100

    ES 02

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,18 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
-const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
-
 // Dichiara la funzione qui.
-
-
+function initials (nomi){
+   const iniziali = [];
+    for (let i=0; i< nomi.lenght; i++){
+      iniziali.push(nomi[i].charAt(0));
+    }
+    return iniziali;
+    }
+        
 // Invoca la funzione qui e stampa il risultato in console
-
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+const risultato= initials(names);
+console.log(risultato);
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
